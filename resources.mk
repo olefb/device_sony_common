ab06fcb18435fea46c4d4a1072f413bbd96c9bdf
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-11-03 23:17:38 +0800

    Revert "Build setup_fs"
    * Use fs_mgr to format.
    
    This reverts commit a86009ef2f5dee762c97285ca29bbbb97dd35a0d.
    
    Change-Id: Iadb25a27d821f3b075baef9bf03488ba7ce9131f

--- resources.mk
+++ resources.mk
@@ -18,7 +18,3 @@ PRODUCT_COPY_FILES += \
 # Boot Logo
 PRODUCT_COPY_FILES += \
      device/sony/common/bootlogo/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT).rle:root/logo.rle
-
-# For formatting data after unlock
-PRODUCT_PACKAGES += \
-    setup_fs
